ca6be31031c374be7068af5e079348d55d03f840
marco <marcodecrema@libero.it>
2020-09-01 16:21:16 +0200

    chiudi menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
+// Associo una variabile al tasto hamburger, al menu Hamburger e al tasto chiudi
 var btn = $(".header-right > a");
 var hamMenu = $(".hamburger-menu");
 var close = $(".close");
 
+// Apri menu
 btn.click(function() {
   hamMenu.show();
 });
+
+// Chiudi menu
+close.click(function() {
+  hamMenu.hide();
+})
