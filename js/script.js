d7183b086bb5ac3ed142121991193e3ccaecf1f1
marco <marcodecrema@libero.it>
2020-09-01 16:24:52 +0200

    aggiungo velocità di transizione

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Associo una variabile al tasto hamburger, al menu Hamburger e al tasto chiudi
-var btn = $(".header-right > a");
+var hamBtn = $(".header-right > a");
 var hamMenu = $(".hamburger-menu");
 var close = $(".close");
 
 // Apri menu
 btn.click(function() {
-  hamMenu.show();
+  hamMenu.show(700);
 });
 
 // Chiudi menu
 close.click(function() {
-  hamMenu.hide();
+  hamMenu.hide(500);
 })
